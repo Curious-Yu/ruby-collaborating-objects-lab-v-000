f0e9d1e01e2ee46ccd8eed2f26cd2a42a5b3d10d
Wen Yu <wyu10amy@hotmail.com>
2021-06-08 06:43:55 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,17 +14,25 @@ class Artist
   end
 
   def songs
-    Song.all.select do |song|
-      song.artist == self
-    end
+    Song.all.select { |song| song.artist == self }
   end
 
-  def add_songs
+  def add_song(song)
     song.artist = self
   end
 
-  def find_or_create_by_name(name)
-    
+  def self.find_or_create_by_name(name)
+    @@all.each do |artist|
+      if artist.name == name
+        return artist
+      end
+    end
+    self.new(name)
+  end
+
+  def print_songs
+    Song.all.each { |song| puts song.name if song.artist == self }
+  end
 
 
-end
+end 
